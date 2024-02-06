022c2e5f1988ea5a7ca1c01731134702e8409784
SebZanardo <97170564+SebZanardo@users.noreply.github.com>
2024-02-06 11:17:06 +1100

    Add files via upload

---
+++ Mixed Fraction/main.py
@@ -0,0 +1,10 @@
+with open('mixin.txt', 'r') as input_file:
+    n, d = map(int, input_file.readline().strip().split())
+
+a = int(n/d)
+b = n % d
+
+equation = str(a) if b == 0 else f'{a} {b}/{d}'
+
+with open('mixout.txt', 'w') as output_file:
+    output_file.write(equation)
---
+++ Sitting or Standing?/main.py
@@ -0,0 +1,13 @@
+with open('sitin.txt', 'r') as input_file:
+    r, s = map(int, input_file.readline().strip().split())
+    t = int(input_file.readline())
+
+if r * s >= t:
+    sitting = t
+    standing = 0
+else:
+    sitting = r * s
+    standing = t - r * s
+
+with open('sitout.txt', 'w') as output_file:
+    output_file.write(f'{sitting} {standing}')
