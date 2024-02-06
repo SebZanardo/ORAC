bcfc2775703e551a157cc8b116d42488a4913449
SebZanardo <97170564+SebZanardo@users.noreply.github.com>
2024-02-06 11:09:33 +1100

    Add files via upload

---
+++ Addition/main.py
@@ -0,0 +1,7 @@
+with open('addin.txt', 'r') as input_file:
+    a, b = map(int, input_file.readline().split())
+
+total = a + b
+
+with open('addout.txt', 'w') as output_file:
+    output_file.write(str(total))
